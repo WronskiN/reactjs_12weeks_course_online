620fdd1fe38c1a5f4e7f2bfb5d9c6c023e9c0430
WronskiN <norbertwronski@yahoo.com>
2020-10-24 20:47:07 +0100

    Clear interval in FunctionTimer component

--- week_2/timer/src/FunctionTimer.js
+++ week_2/timer/src/FunctionTimer.js
@@ -3,7 +3,10 @@ import React, { useState, useEffect } from "react";
 const FunctionTimer = () => {
   const [time, setTime] = useState(0);
 
-  useEffect(() => setInterval(() => setTime(time + 1), 1000));
+  useEffect(() => {
+    const interval = setInterval(() => setTime((time) => time + 1), 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return <div>Function Timer: {time}</div>;
 };
