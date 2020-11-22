6410d45e534116ca997830884d6cba7fe23ead0b
WronskiN <norbertwronski@yahoo.com>
2020-11-22 21:50:04 +0000

    The bydget component bgc depends on status

--- week_4/expenses-calculator/src/components/Budget/Budget.js
+++ week_4/expenses-calculator/src/components/Budget/Budget.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Budget({ budget }) {
-  return <div className="budget">{budget}</div>;
+  return (
+    <div
+      className={
+        budget < 0 ? 'budget budget--negative' : 'budget budget--positive'
+      }
+    >
+      {budget}
+    </div>
+  );
 }
 
 export default Budget;
