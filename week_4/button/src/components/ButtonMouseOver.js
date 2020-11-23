922504c577099afc1990eea6463d7e6b7373abfe
WronskiN <norbertwronski@yahoo.com>
2020-11-23 16:00:18 +0000

    Add logic into ButtonMouseOver component

--- week_4/button/src/components/ButtonMouseOver.js
+++ week_4/button/src/components/ButtonMouseOver.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
 function ButtonMouseOver() {
-  return <button>Mouse over</button>;
+  const buttonChange = useRef();
+  const [mouseOver, setMouseOver] = useState(false);
+
+  useEffect(() => {
+    if (mouseOver) {
+      buttonChange.current.style.color = '#fff';
+      buttonChange.current.style.backgroundColor = 'red';
+    } else {
+      buttonChange.current.style.color = '#000';
+      buttonChange.current.style.backgroundColor = 'blue';
+    }
+  });
+
+  return (
+    <button
+      ref={buttonChange}
+      onMouseOver={() => setMouseOver(!mouseOver)}
+      onMouseOut={() => setMouseOver(!mouseOver)}
+    >
+      Mouse over
+    </button>
+  );
 }
 
 export default ButtonMouseOver;
