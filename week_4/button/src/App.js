770fcae4a9edefaf3639cfb39dea0ac6df48660c
WronskiN <norbertwronski@yahoo.com>
2020-11-23 14:59:12 +0000

    Create Button component

--- week_4/button/src/App.js
+++ week_4/button/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Button from './components/Button';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <h1>Button project</h1>
+      <Button />
     </div>
   );
 }
