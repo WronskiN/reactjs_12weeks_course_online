165f818e79548e626ad1d932f4bf0a0b41664bd3
WronskiN <norbertwronski@yahoo.com>
2020-11-24 20:41:04 +0000

    Create Button and Dialog components

--- week_5/dialog/src/App.js
+++ week_5/dialog/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Button from './components/Button';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <h1>Dialog</h1>
+      <Button />
     </div>
   );
 }
---
+++ week_5/dialog/src/components/Button/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+
+function Button() {
+  return <button>Open Dialog</button>;
+}
+
+export default Button;
