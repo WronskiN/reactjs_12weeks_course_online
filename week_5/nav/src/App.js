2a370533c1c5d018d2074b9d3aeee656ea7602c2
WronskiN <norbertwronski@yahoo.com>
2020-11-26 14:01:50 +0000

    Delete commented lines in App.js

--- week_5/nav/src/App.js
+++ week_5/nav/src/App.js
@@ -10,26 +10,6 @@ function App() {
   const handleClick = () => setToggleMenu(!toggleMenu);
   const handleInnerButton = () => setToggleMenu(false);
 
-  // const [error, setError] = useState(null);
-  // const [isLoaded, setIsLoaded] = useState(false);
-  // const [items, setItems] = useState([]);
-
-  // useEffect(() => {
-  //   fetch('https://randomuser.me/api/?results=10')
-  //     .then((res) => res.json())
-  //     .then(
-  //       (result) => {
-  //         setIsLoaded(true);
-  //         setItems(result);
-  //         console.log(result);
-  //       },
-  //       (error) => {
-  //         setIsLoaded(true);
-  //         setError(error);
-  //       }
-  //     );
-  // }, []);
-
   return (
     <div className='App'>
       <Router>
