02a09424ff702b7f38498b347e266e2fe1aceb86
WronskiN <norbertwronski@yahoo.com>
2020-12-01 12:07:15 +0000

    Replace fetch url for json file

--- week_5/nav/src/containers/Users/Users.js
+++ week_5/nav/src/containers/Users/Users.js
@@ -4,17 +4,17 @@ import './users.css';
 import { UsersList } from '../../components/UsersList';
 // import { UserCard } from '../../components/User';
 
-// const URL = '../../../public/users.json';
+const URL = './users.json';
 
 function Users() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [users, setUsers] = useState([]);
   const [filteredUsers, setFilteredUsers] = useState();
-  const API_URL = 'https://randomuser.me/api/?results=10';
+  // const API_URL = 'https://randomuser.me/api/?results=10';
 
   const loadData = async () => {
     setIsLoaded(true);
-    const response = await fetch(API_URL);
+    const response = await fetch(URL);
     const data = await response.json();
     setUsers(data.results);
     return setIsLoaded(!isLoaded);
