3fcada167efeb6846cd1b876466626c70549012b
WronskiN <norbertwronski@yahoo.com>
2020-12-03 19:29:50 +0000

    Update uploadUser function

--- week_5/nav/src/components/User/User/User.js
+++ week_5/nav/src/components/User/User/User.js
@@ -2,35 +2,52 @@ import React, { useState, useEffect } from 'react';
 import { UserCard } from '../';
 import { useParams } from 'react-router-dom';
 
+// fetch(URL)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const filteredUser = data.results.filter(
+//       (user) => user.id === parseInt(id, 10)
+//     );
+//     setUser(filteredUser);
+//     setIsLoaded(true);
+//   })
+//   .catch((error) => {
+//     setError(true);
+//     setIsLoaded(false);
+//   });
+
 const URL = '/users.json';
 
 function User() {
   const { id } = useParams();
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [hasError, setError] = useState(false);
-  console.log(`numer id to: ${id}`);
+  console.log(typeof id);
+
+  const uploadUser = async () => {
+    setIsLoaded(true);
+    const response = await fetch(URL);
+    const data = await response.json();
+    const filteredUser = data.results.filter(
+      (user) => user.id === parseInt(id, 10)
+    );
+    // setTimeout(() => setUser(filteredUser), 2000);
+    setUser(filteredUser);
+    // console.log(user);
+    // return setIsLoaded(!isLoaded);
+  };
 
   useEffect(() => {
-    fetch(URL)
-      .then((response) => response.json())
-      .then((data) => {
-        const filteredUser = data.results.filter((user) => user.id === id);
-        setUser(filteredUser);
-        setIsLoaded(true);
-      })
-      .catch((error) => {
-        setError(true);
-        setIsLoaded(false);
-      });
-  }, [id]);
+    uploadUser();
+  }, []);
 
   return (
     <div className='section section__users'>
       {console.log(`filtered user ${user}`)}
       {isLoaded && <p>Loading data</p>}
       {hasError && <p>Error occurred</p>}
-      {user ? (
+      {/* {user ? (
         <UserCard
           key={user.id}
           name={user.name.first}
@@ -43,7 +60,7 @@ function User() {
         />
       ) : (
         <p>Data is not ready</p>
-      )}
+      )} */}
     </div>
   );
 }
