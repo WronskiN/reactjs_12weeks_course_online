9b6a48871923932dff6ba6066d31a95822c8003c
WronskiN <norbertwronski@yahoo.com>
2020-12-13 20:31:51 +0000

    Add Placeholder component into App.js

--- week_7/users/src/App.js
+++ week_7/users/src/App.js
@@ -1,24 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-
 import UserList from './components/UserList/UserList';
+
+import ReactPlaceholder from 'react-placeholder';
+import 'react-placeholder/lib/reactPlaceholder.css';
 
 const URL = 'https://randomuser.me/api/?results=10';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [state, setState] = useState(false);
 
   useEffect(() => {
     axios.get(URL).then((result) => {
       setUsers(result.data.results);
+      setState(true);
     });
   }, []);
 
   return (
     <div className='App'>
       <h1>Users</h1>
-      <UserList users={users} />
+      <ReactPlaceholder
+        ready={state}
+        type='media'
+        rows={6}
+        showLoadingAnimation
+      >
+        <UserList users={users} />
+      </ReactPlaceholder>
     </div>
   );
 }
