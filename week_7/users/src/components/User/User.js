53f80ae3f1f515c42c261ea86646ac29202ff871
WronskiN <norbertwronski@yahoo.com>
2020-12-13 19:36:36 +0000

    Create UserDetails component

--- week_7/users/src/components/User/User.js
+++ week_7/users/src/components/User/User.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function User({ user }) {
+  const address = user.location;
+  const name = user.name;
   return (
     <div className='user__card'>
       <img src={user.picture.medium} alt='user profile' />
       <article>
-        <p>{`Name: ${user.name.first} ${user.name.last}`}</p>
-        <p>{`Address: ${user.location.street.number} ${user.location.street.name}`}</p>
+        <h3>
+          <Link to='/'>
+            {name
+              ? `${user.name.first} ${user.name.last}`
+              : 'The name was not found'}
+          </Link>
+        </h3>
+        <p>
+          {address
+            ? `Address: ${user.location.street.number} ${user.location.street.name}`
+            : 'The address was not found'}
+        </p>
         <p>{`Email: ${user.email}`}</p>
-        <p>{`Registered date: ${new Date(user.registered.date)
+        <p>{`Registered: ${new Date(user.registered.date)
           .toLocaleDateString('en-UK')
           .split('/:| /')}`}</p>
       </article>
--- week_7/users/src/components/UserList/UserList.js
+++ week_7/users/src/components/UserList/UserList.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import User from '../User/User';
+import UserDetails from '../UserDetails/UserDetails';
 
 function UserList({ users }) {
   let usersList = users;
   return (
-    <ul className='user__list'>
-      {usersList.map((user, index) => (
-        <User key={`id-${index}`} user={user} />
-      ))}
-    </ul>
+    <Router>
+      <Switch>
+        <Route path='/users/:id'>
+          <UserDetails />
+        </Route>
+        <Route path='/'>
+          <ul className='user__list'>
+            {usersList.map((user, index) => (
+              <User key={`id-${index}`} user={user} />
+            ))}
+          </ul>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
