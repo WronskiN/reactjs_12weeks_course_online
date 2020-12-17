165c7f1b68b2fa33e59daff0a0ffa511ffd0481c
WronskiN <norbertwronski@yahoo.com>
2020-12-17 21:30:44 +0000

    Create component Buttons

--- week_8/test_one/src/App.js
+++ week_8/test_one/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sum from './components/Sum/Sum';
+import Buttons from './components/Buttons/Buttons';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Sum />
+      <Buttons start={0} />
     </div>
   );
 }
---
+++ week_8/test_one/src/components/Buttons/Buttons.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function Buttons(props) {
+  const [counter, setCounter] = useState(props.start);
+  const [inputValue, setInputValue] = useState(0);
+
+  const handleIncrementButton = () => {
+    setCounter(counter + 1);
+  };
+  const handleDecrementButton = () => {
+    setCounter(counter - 1);
+  };
+  const handleInput = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
+  };
+  const handleChangeButton = () => {
+    setCounter(inputValue);
+    setInputValue(0);
+  };
+  const handleResetButton = () => {
+    setCounter(props.start);
+  };
+
+  return (
+    <div>
+      <p>Counter: {counter}</p>
+      <input type='number' value={inputValue} onChange={handleInput} />
+      <button onClick={handleChangeButton}>Change</button>
+      <button onClick={handleDecrementButton}>-</button>
+      <button onClick={handleIncrementButton}>+</button>
+      <button onClick={handleResetButton}>Reset</button>
+      <p>{inputValue}</p>
+    </div>
+  );
+}
+
+export default Buttons;
