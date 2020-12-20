c1fb1cb38e5f3d56b56b386b3cf6b91a31c216ce
WronskiN <norbertwronski@yahoo.com>
2020-12-20 02:32:32 +0000

    remove users array

--- week_7/users/src/components/UserList/UserList.test.js
+++ week_7/users/src/components/UserList/UserList.test.js
@@ -3,22 +3,9 @@ import { shallow } from 'enzyme';
 import UserList from './UserList';
 
 describe('UserList component', () => {
-  const users = [
-    'one',
-    'two',
-    'three',
-    'four',
-    'five',
-    'six',
-    'seven',
-    'eight',
-    'nine',
-    'ten',
-  ];
   it('UserList length should be 10', () => {
-    const list = shallow(<UserList users={users} />);
+    const list = shallow(<UserList />);
     const userList = list.find('ul');
     expect(userList).toHaveLength(10);
-    // expect(userList.length).toBe(10);
   });
 });
